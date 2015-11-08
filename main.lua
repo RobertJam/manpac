f27dbff954b7848d0590167300c4f6bd88e75c23
patzy <morgan.veyret@gmail.com>
2015-11-08 17:14:32 +0100

    added update func

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 
+game = {time=0}
+
+-- update a frame
+function love.update(dt)
+   game.time = game.time + dt
+end
+
 -- draw a frame
 function love.draw()
-   love.graphics.print("Hello les gens",100,100)
+   love.graphics.print("Hello les gens...le temps passe...",100,100)
+   love.graphics.print(game.time,100,115)
 end

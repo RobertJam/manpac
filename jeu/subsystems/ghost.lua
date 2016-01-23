f43283e723d310739cd389bcadc7d51cec1c1eb6
patzy <morgan.veyret@gmail.com>
2016-01-23 16:55:32 +0100

    Wrote some text in ghost.lua

--- jeu/subsystems/ghost.lua
+++ jeu/subsystems/ghost.lua
@@ -1,13 +1,15 @@
 local ghost = {}
 
-function ghost.init_entity(self)
+function ghost.init_entity(self,cfg)
+   self.nbarriers = cfg.max_barriers or 3
 end
 
 function ghost.init_system()
 end
 
-function ghost.enter_collision(self,other)
-   print("Ghost colliding")
+function ghost.create_barrier(self)
+   if self.nbarriers == 0 then return end
+   self.nbarriers = self.nbarriers - 1
 end
 
 return ghost

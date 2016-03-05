af5bbaa87008f02f0d6288a2bcdc1f76a46d5456
mlg05 <mylene.lorre@netcourrier.com>
2016-03-05 14:17:29 +0100

    début

--- libs/animation.lua
+++ libs/animation.lua
@@ -4,12 +4,26 @@ function love.load()
   fantome_IA = love.graphics.newImage('../assets/sprites/fantome_IA.png')
 
                          -- frame, image,    offsets, border
-  local walk = anim8.newGrid(52,52, 11628,57,   3,3,     1)
+  local walk = anim8.newGrid(52,52, 11628,57,   0,0,     1)
 
-  walk = {
+  walk_down = {
                      -- type    -- frames                   --default delay
-    anim8.newAnimation(walk_left(52,'52-68'), 0.2),
-	anim8.newAnimation(walk_right(86,'86-102'), 0.2),
-	anim8.newAnimation(walk_up(120,'120-136'), 0.2),
-	anim8.newAnimation(walk_down(18,'18-34'), 0.2),
+    anim8.newAnimation(walk('18-34',2), 0.2),
   }
+  walk_left = {
+                     -- type    -- frames                   --default delay
+    anim8.newAnimation(walk('52-68',2), 0.2),
+  }
+  walk_right = {
+                     -- type    -- frames                   --default delay
+    anim8.newAnimation(walk('86-102',2), 0.2),
+  }
+  walk_up = {
+                     -- type    -- frames                   --default delay
+    anim8.newAnimation(walk('120-136',2), 0.2),
+  }
+  	
+	
+	
+  
+end

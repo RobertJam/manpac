a3b7a98ce3365c104d6acdd2f8d1ed162e96174d
mlg05 <mylene.lorre@netcourrier.com>
2016-03-05 17:03:35 +0100

    modification animation

--- libs/animation.lua
+++ libs/animation.lua
@@ -1,4 +1,5 @@
 local anim8 = require 'anim8'
+local fantome
 
 function love.load()
   fantome_IA = love.graphics.newImage('../assets/sprites/fantome_IA.png')
@@ -6,58 +7,40 @@ function love.load()
                          -- frame, image,    offsets, border
   local grid = anim8.newGrid(52,52, 11628,57,   0,0,     0)
 
-  -- walk
-  walk_down = 
-  {
-                     -- type    -- frames                   --default delay
-    anim8.newAnimation(grid('18-34',10), 0.2),
-  }
-  walk_left = 
-  {
-    anim8.newAnimation(grid('52-68',10), 0.2),
-  }
-  walk_right = 
-  {
-    anim8.newAnimation(grid('86-102',10), 0.2),
-  }
-  walk_up = 
-  {
-    anim8.newAnimation(grid('120-136',10), 0.2),
-  }
-  -- iddle
-  iddle_down = 
-  {
-    anim8.newAnimation(grid('1-17',10), 0.2),
-  }
-  iddle_left = 
-  {
-    anim8.newAnimation(grid('35-51',10), 0.2),
-  }
-  iddle_right = 
-  {
-    anim8.newAnimation(grid('86-102',10), 0.2),
-  }
-  iddle_up = 
-  {
-    anim8.newAnimation(grid('103-119',10), 0.2),
-  } 
-  -- gausse
-  gausse_down = 
-  {
-    anim8.newAnimation(grid('137-153',10), 0.2),
-  }
-  gausse_left = 
-  {
-    anim8.newAnimation(grid('154-170',10), 0.2),
-  }
-  gausse_right = 
-  {
-    anim8.newAnimation(grid('171-187',10), 0.2),
-  }
-  gausse_up = 
-  {
-    anim8.newAnimation(grid('188-204',10), 0.2),
+  fantome = {
+	fantome_IA = fantome_IA,
+	x = 0,
+	y = 0,
+	speed = 10,
+	animation = {
+		--walk
+		walk_down = anim8.newAnimation('loop', grid('18-34',10), 1.0),
+		walk_left = anim8.newAnimation('loop', grid('52-68',10), 1.0),
+		walk_right = anim8.newAnimation('loop', grid('86-102',10), 1.0),
+		walk_up = anim8.newAnimation('loop', grid('120-136',10), 1.0),
+		--iddle
+		iddle_down = anim8.newAnimation('loop', grid('1-17',10), 1.0),
+		iddle_left = anim8.newAnimation('loop', grid('35-51',10), 1.0),
+		iddle_right = anim8.newAnimation('loop', grid('86-102',10), 1.0),
+		iddle_up = anim8.newAnimation('loop', grid('103-119',10), 1.0),
+		--gausse
+		gausse_down = anim8.newAnimation('loop', grid('137-153',10), 1.0),
+		gausse_left = anim8.newAnimation('loop', grid('154-170',10), 1.0),
+		gausse_right = anim8.newAnimation('loop', grid('171-187',10), 1.0),
+		gausse_up = anim8.newAnimation('loop', grid('188-204',10), 1.0),
+	}
   } 	
 	
-  
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+
 end

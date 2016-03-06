fd55043f144b497b9802c5b743a88cf9b2da46c6
Mini-pen <cyrille.baudouin@gmail.com>
2016-03-06 12:35:59 +0100

    Add maps

--- jeu/gui/quick_start.lua
+++ jeu/gui/quick_start.lua
@@ -17,13 +17,15 @@ function gui.quick_start.Load()
 	gui.quick_start.panel = gui.quick_start.panel:SetPos(0, 0)
 	
 	gui.quick_start.multichoice = loveframes.Create("multichoice", frame)
-      gui.quick_start.multichoice:SetSize(200, 30)
-      gui.quick_start.multichoice:SetPos(width / 2 - 100, height / 2 - 15 - object_spaing - 30)
-      gui.quick_start.multichoice:AddChoice("sewers")
-      gui.quick_start.multichoice:AddChoice("map_V4")
-      gui.quick_start.multichoice:AddChoice("map_V5")
-      gui.quick_start.multichoice.OnChoiceSelected = function()
-      gui.map = "assets/maps/" .. gui.quick_start.multichoice:GetChoice() .. ".lua"
+    gui.quick_start.multichoice:SetSize(200, 30)
+    gui.quick_start.multichoice:SetPos(width / 2 - 100, height / 2 - 15 - object_spaing - 30)
+    gui.quick_start.multichoice:AddChoice("sewers")
+    gui.quick_start.multichoice:AddChoice("map_V4")
+    gui.quick_start.multichoice:AddChoice("map_V5")
+    gui.quick_start.multichoice:AddChoice("giant_rock_maze")
+    gui.quick_start.multichoice:AddChoice("giant_earth_maze")
+    gui.quick_start.multichoice.OnChoiceSelected = function()
+    gui.map = "assets/maps/" .. gui.quick_start.multichoice:GetChoice() .. ".lua"
 	end
 
 	local start_button = loveframes.Create("button", gui.quick_start.panel)
@@ -49,7 +51,11 @@ function gui.quick_start.Launch()
          audio.LoopMusic(audio.sounds.map_music2, 0.05)
       elseif map_choice == "map_V4" then
          audio.LoopMusic(audio.sounds.map_music1, 0.05)
-	   elseif map_choice == "map_V5" then
+	  elseif map_choice == "map_V5" then
+         audio.LoopMusic(audio.sounds.map_music1, 0.05)
+	  elseif map_choice == "giant_rock_maze" then
+         audio.LoopMusic(audio.sounds.map_music2, 0.05)
+	  elseif map_choice == "giant_earth_maze" then
          audio.LoopMusic(audio.sounds.map_music1, 0.05) 
       else
          audio.LoopMusic(audio.sounds.map_music2, 0.05)

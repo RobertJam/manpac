eeae7fce3755c2814dad46fe3d456dab79e2801a
patzy <morgan.veyret@gmail.com>
2016-03-06 12:02:41 +0100

    Changed end of game key

--- jeu/game_over.lua
+++ jeu/game_over.lua
@@ -14,11 +14,11 @@ function state.draw()
    else
       love.graphics.print("Defeat !!!!",100,100)
    end
-   love.graphics.print("Press ESCAPE to return to main menu",100,120)
+   love.graphics.print("Press space to return to main menu",100,120)
 end
 
 function state.keypressed(key, isrepeat)
-   if love.keyboard.isDown("escape") then
+   if love.keyboard.isDown(" ") then
       gs.switch("jeu/start")
    end
 end
